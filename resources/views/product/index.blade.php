7721f735abcfa26169c727a1bf194aecb25a0bc5
Ashish Shukla <akshkgd@gmail.com>
2020-10-28 14:39:14 +0530

    Bug Fix

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -13,13 +13,7 @@
         </div>
     </div>
 
-    <div class="flash-message">
-        @foreach (['danger', 'warning', 'success', 'info'] as $msg)
-        @if(Session::has('alert-' . $msg))
-
-        <p class="alert" alert-{{ $msg }}" role="alert">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
-        @endif
-        @endforeach
+   
 
         <table class="table table-bordered">
             <tr>
